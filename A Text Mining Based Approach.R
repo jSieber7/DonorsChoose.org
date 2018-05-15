514e160fcd7c071f4dc2f13a7b4616986b80f672
Jacob Sieber <Jakestr9000@gmail.com>
2018-05-14 21:12:11 -0400

    Creating a Recommendation Approach
    The new direction of this project is now in the readme.

--- A Text Mining Based Approach.R
+++ A Text Mining Based Approach.R
@@ -1,10 +1,16 @@
 # In this script, I attempt to further differentiate projects by text
+# Heavily uses code from Dr. Reza Mousavi, a professor at UNCC
+
+
+# Self Note: Going to switch the topic modeling code using https://www.kaggle.com/rtatman/nlp-in-r-topic-modelling/code as a reference 
 
 library(data.table)
 library(dplyr)
 library(tidytext)
 library(wordcloud)
 library(ggplot2)
+library(tidyr)
+
 
 
 # additional column to investigate: `Project Subject Category Tree`
@@ -29,7 +35,7 @@ text_df %>%
 
 text_df %>%
   count(word, sort = TRUE) %>%
-  filter(n > 1000) %>%
+  top_n(20,wt = n) %>%
   mutate(word = reorder(word, n)) %>%
   ggplot(aes(word, n)) +
   geom_bar(stat = "identity") +
@@ -57,6 +63,64 @@ essay_words <- left_join(text_df_2, total_words)
 essay_words  <- essay_words  %>%
   bind_tf_idf(word, `Project ID`, n)
 
+head(essay_words,20)
+
 text_df %>%
   count(word) %>%
   with(wordcloud(word, n, max.words = 100))
+
+
+
+# Topic modeling
+
+library(RTextTools)
+library(tm)
+library(wordcloud)
+library(topicmodels)
+library(slam)
+
+data <- text_data[1:1000,]
+
+clean_words <-removeWords(data$`Project Essay`, c('<!--donotremoveessaydivider-->','donotremoveessaydivider','--donotremoveessaydivider--',
+                                                  '<!---->')) 
+
+data$`Project Essay` <- clean_words
+
+
+
+(head(data,1))
+
+corpus <- Corpus(VectorSource(data$`Project Essay`), readerControl=list(language="en"))
+
+tweet_dtm <- DocumentTermMatrix(corpus, control = list(stopwords = TRUE, minWordLength = 3, removeNumbers = TRUE, removePunctuation = TRUE))
+tweet_dtm
+
+
+lda <- LDA(tweet_dtm, k = 3, control = list(seed = 1234))
+lda
+
+lda_td <- tidy(lda)
+lda_td
+
+
+top_terms <- lda_td %>%
+  group_by(topic) %>%
+  top_n(10, beta) %>%
+  ungroup() %>%
+  arrange(topic, -beta)
+
+top_terms %>%
+  mutate(term = reorder(term, beta)) %>%
+  ggplot(aes(term, beta, fill = factor(topic))) +
+  geom_bar(stat = "identity", show.legend = FALSE) +
+  facet_wrap(~ topic, scales = "free") +
+  coord_flip()
+
+beta_spread <- lda_td %>%
+  mutate(topic = paste0("topic", topic)) %>%
+  spread(topic, beta) %>%
+  filter(topic1 > .001 | topic2 > .001) %>%
+  mutate(dif_12 = abs(topic1 - topic2)) %>%
+  arrange(desc(dif_12))
+
+beta_spread
--- README.md
+++ README.md
@@ -2,4 +2,12 @@
 
 This project seeks to analyzing DonorsChoose.org in order to generate business insights that will drive more dollars into the hands of teachers across America. The tool of choice is R. All data taken from https://www.kaggle.com/donorschoose/io.
 
-The code that will be used in the final kernel are inside IPYNB files.
+The code that will be used in the final kernel is inside the 'Telling a Story' IPYNB file, other files serve as exploration and modeling at the moment.
+
+
+##Current Approach:
+
+The greatest chance in building a good recommendation system as of now seems to be generating a discriptive set of features about a project, and recommending other projects to users based on a distance measure (cosign, Jaccard, ect.)  between projects. Also, grouping similar users based on a discriptive set of features (PCA or SVD may be required) with a distance masure may be useful aswell.  Creating decent clusters out of this data seems doubtful, but will make an attempt. My current activity is using topic modeling to add a new discriptive feature for each project.
+
+
+
